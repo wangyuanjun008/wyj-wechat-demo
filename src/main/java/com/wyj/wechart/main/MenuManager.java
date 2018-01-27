bc38c974bfd73ab98ac2b47d56c6faeef6f935dc
wangyuanjun008 <1098345452@qq.com>
2018-01-27 14:44:46 +0800

    修改测试菜单项

--- src/main/java/com/wyj/wechart/main/MenuManager.java
+++ src/main/java/com/wyj/wechart/main/MenuManager.java
@@ -47,61 +47,64 @@ public class MenuManager {
     private static Menu getMenu() {
         CommonButton btn11 = new CommonButton();
         btn11.setName("天气预报");
-        btn11.setType("click");
+        btn11.setType("view");
         btn11.setKey("11");
+        btn11.setUrl("http://www.weather.com.cn/weather/101190101.shtml");
 
         CommonButton btn12 = new CommonButton();
         btn12.setName("公交查询");
-        btn12.setType("click");
+        btn12.setType("view");
         btn12.setKey("12");
-
+        btn12.setUrl("http://www.gongjiao.com/");
+        
         CommonButton btn13 = new CommonButton();
-        btn13.setName("周边搜索");
-        btn13.setType("click");
+        btn13.setName("百度地图");
+        btn13.setType("view");
         btn13.setKey("13");
-
+        btn13.setUrl("https://map.baidu.com/");
+        
         CommonButton btn14 = new CommonButton();
-        btn14.setName("历史上的今天");
+        btn14.setName("滴滴出行");
         btn14.setType("click");
         btn14.setKey("14");
 
         CommonButton btn21 = new CommonButton();
-        btn21.setName("歌曲点播");
+        btn21.setName("csdn");
         btn21.setType("click");
         btn21.setKey("21");
 
         CommonButton btn22 = new CommonButton();
-        btn22.setName("经典游戏");
+        btn22.setName("博客园");
         btn22.setType("click");
         btn22.setKey("22");
 
         CommonButton btn23 = new CommonButton();
-        btn23.setName("美女电台");
+        btn23.setName("开发头条");
         btn23.setType("click");
         btn23.setKey("23");
 
         CommonButton btn24 = new CommonButton();
-        btn24.setName("人脸识别");
+        btn24.setName("云栖社区");
         btn24.setType("click");
         btn24.setKey("24");
 
         CommonButton btn25 = new CommonButton();
-        btn25.setName("聊天唠嗑");
+        btn25.setName("github");
         btn25.setType("click");
         btn25.setKey("25");
 
         CommonButton btn31 = new CommonButton();
-        btn31.setName("Q友圈");
+        btn31.setName("淘宝网");
         btn31.setType("click");
         btn31.setKey("31");
 
         CommonButton btn32 = new CommonButton();
-        btn32.setName("电影排行榜");
+        btn32.setName("电影天堂");
         btn32.setType("click");
         btn32.setKey("32");
 
         CommonButton btn33 = new CommonButton();
-        btn33.setName("幽默笑话");
+        btn33.setName("小游戏");
         btn33.setType("click");
         btn33.setKey("33");
 
@@ -111,18 +114,17 @@ public class MenuManager {
          */
         
         ComplexButton mainBtn1 = new ComplexButton();
-        mainBtn1.setName("生活助手");
+        mainBtn1.setName("生活便利");
         //一级下有4个子菜单
         mainBtn1.setSub_button(new CommonButton[] { btn11, btn12, btn13, btn14 });
-
         
         ComplexButton mainBtn2 = new ComplexButton();
-        mainBtn2.setName("休闲驿站");
+        mainBtn2.setName("学习社区");
         mainBtn2.setSub_button(new CommonButton[] { btn21, btn22, btn23, btn24, btn25 });
 
         
         ComplexButton mainBtn3 = new ComplexButton();
-        mainBtn3.setName("更多体验");
+        mainBtn3.setName("娱乐一下");
         mainBtn3.setSub_button(new CommonButton[] { btn31, btn32, btn33 });
 
         
